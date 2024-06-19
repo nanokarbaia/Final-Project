109dde5a4a9b165ba2e68ee64cf24fa3424bc18e
Nano_Karbaia <nanokarbaia@gmail.com>
2024-06-19 16:45:19 +0400

    new commit

--- myweb/base/models.py
+++ myweb/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Pict(models.Model):
+    picture=models.CharField(max_length=400)
+    # name=models.CharField(max_length=200)
+    # author=models.CharField(max_length=200)
+    # description=models.TextField(max_length=500)
+    # content=models.CharField(max_length=200)
+
+    # def __str__(self):
+    #     return f"{self.name} _ {self.author}"
--- myweb/base/views.py
+++ myweb/base/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Pict
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    pictures=Pict.objects.all()
+    # for picture in pictures:
+    #     print(picture)
+
+    context={"pictures": pictures}
+
+    return render(request, 'base/home.html', context)
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'base/about.html')
 
 
 
