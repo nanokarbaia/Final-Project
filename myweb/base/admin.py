78807f991a2094e013bbd2c924d36248dc18d890
Nano_Karbaia <nanokarbaia@gmail.com>
2024-06-26 00:51:22 +0400

    new commit

--- myweb/base/admin.py
+++ myweb/base/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
 from .models import Pict
+
+from .models import JobCategory, JobListing, Applicant, User
+
+
 # Register your models here.
 
 admin.site.register(Pict)
+
+admin.site.register(JobCategory)
+
+admin.site.register(JobListing)
+
+admin.site.register(Applicant)
+
+# admin.site.register(User)
+
+
+
+
+
+
+
+
+# @admin.register(JobCategory)
+# class JobCategoryAdmin(admin.ModelAdmin):
+#     list_display = ['title']
+#
+# @admin.register(JobListing)
+# class JobListingAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'company', 'location']
+#     list_filter = ['category', 'location']
+#     search_fields = ['title', 'company', 'description']
+#
+# @admin.register(Applicant)
+# class ApplicantAdmin(admin.ModelAdmin):
+#     list_display = ['job_listing', 'user']
+#     list_filter = ['job_listing', 'user']
+#     search_fields = ['user__username', 'job_listing__title']
 
 
--- myweb/base/models.py
+++ myweb/base/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -9,5 +11,39 @@ class Pict(models.Model):
     # description=models.TextField(max_length=500)
     # content=models.CharField(max_length=200)
 
-    # def __str__(self):
+  # def __str__(self):
     #     return f"{self.name} _ {self.author}"
+
+class JobCategory(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+class JobListing(models.Model):
+    title = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+    requirements = models.TextField()
+    category = models.ForeignKey(JobCategory, on_delete=models.CASCADE)
+    # published_at = models.DateTimeField(auto_now_add=True)
+    # filled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Applicant(models.Model):
+    job_listing = models.ForeignKey(JobListing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # resume = models.FileField(upload_to='resumes/')
+    cover_letter = models.TextField()
+    # applied_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.job_listing.title}"
+
+# class User(AbstractUser):
+#     job_listing=models.ManyToManyField(JobListing, related_name='job_listing', blank=True)
+#
+#
